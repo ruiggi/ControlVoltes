6626b75995141e4ebfc6447290c9f7b514c5bf67
Ruiggi <70644700+ruiggi@users.noreply.github.com>
2025-10-20 15:15:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,42 @@
-const CACHE_NAME = 'control-vueltas-v1';
-const urlsToCache = [
-  '/ControlVueltas/',
-  '/ControlVueltas/index.html',
-  '/ControlVueltas/crono192x192.png'
+const CACHE_NAME = 'control-voltes-cache-v1';
+const URLS_TO_CACHE = [
+  '/ControlVoltes/',
+  '/ControlVoltes/ControlVoltes.html',
+  '/ControlVoltes/manifest.json',
+  '/ControlVoltes/icons/icon-192.svg',
+  '/ControlVoltes/icons/icon-512.svg',
+  '/ControlVoltes/icons/maskable.svg'
 ];
 
-// Instalar el Service Worker
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-// Activar el Service Worker
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+    ))
   );
+  self.clients.claim();
 });
 
-// Interceptar peticiones
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(() => {});
+      return res;
+    }).catch(() => cached))
   );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
